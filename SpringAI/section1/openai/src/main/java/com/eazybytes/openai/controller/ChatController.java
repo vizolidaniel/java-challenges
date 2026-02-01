21c256cb334cd9e70a09d39c5cbb663b3c87532c
Daniel Vizoli <vizoli.daniel96@gmail.com>
2026-02-01 20:39:20 -0300

    Spring AI start

--- SpringAI/section1/openai/src/main/java/com/eazybytes/openai/controller/ChatController.java
+++ SpringAI/section1/openai/src/main/java/com/eazybytes/openai/controller/ChatController.java
@@ -1,4 +1,23 @@
 package com.eazybytes.openai.controller;
 
-public class Controller {
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ChatController {
+
+    private final ChatClient chatClient;
+
+    public ChatController(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
+
+    @GetMapping("/chat")
+    public String chat (@RequestParam("message") String message){
+        return chatClient.prompt(message).call().content();
+    }
 }
